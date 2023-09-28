18d17947a63bc26d1497a650c20a6dcc08aa823e
Oskar Baumann <oskar.baumann@vectralis.com>
2023-09-28 16:01:54 -0600

    Some random changes

---
+++ BlazorTodoApp/BlazorBoards.cs
@@ -0,0 +1,39 @@
+﻿public class ChecklistItem
+{
+    public bool IsDone { get; set; }
+    public string Title { get; set; }
+
+    public ChecklistItem(string title)
+    {
+        Title = title;
+        IsDone = false;
+    }
+}
+
+public class TaskItem
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime? Deadline { get; set; }
+    public List<string> Labels { get; set; }
+    public List<ChecklistItem> Checklist { get; set; }
+
+    public TaskItem(string title)
+    {
+        Title = title;
+        Labels = new List<string>();
+        Checklist = new List<ChecklistItem>();
+    }
+}
+
+public class Board
+{
+    public string Title { get; set; }
+    public List<TaskItem> Tasks { get; set; }
+
+    public Board(string title)
+    {
+        Title = title;
+        Tasks = new List<TaskItem>();
+    }
+}
