6a5521291613612e61d3d2c99517ffc6aba1003e
Oskar Baumann <oskar.baumann@vectralis.com>
2023-09-28 13:58:25 -0600

    Add project files.

---
+++ BlazorTodoApp/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿public class TodoItem
+{
+    public required string Title { get; set; }
+    public bool IsDone { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType()) return false;
+        var compare = (TodoItem)obj;
+        return Title.Equals(compare.Title) && IsDone.Equals(compare.IsDone);
+    }
+}
