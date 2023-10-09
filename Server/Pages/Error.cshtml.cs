526293afefa5bcb28e8bbdaf5862a492a0901812
Oskar Baumann <oskar.baumann@vectralis.com>
2023-10-09 17:26:07 -0600

    Remove server project and rename

--- Server/Pages/Error.cshtml.cs
+++ Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace BlazorBoards.Pages
+namespace BlazorBoards.Server.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,29 +1,67 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
+app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+var labels = new List<Label>() { new Label("prio1", "#000000", "#AA2200") };
+
+app.MapGet("/api/labels", () =>
+{
+    return TypedResults.Ok(labels);
+})
+.WithName("GetLabels")
+.WithOpenApi();
+
+app.MapPut("/api/labels", (List<Label> recievedLabels) =>
+{
+    labels = recievedLabels;
+    return TypedResults.Ok();
+})
+.WithName("PostLabels")
+.WithOpenApi();
+
+app.MapDelete("/api/labels/{id}", Results<Ok, NotFound> (string id) =>
+{
+    var labelToRemove = labels.Find(l => l.Id == id);
+    if (labelToRemove == null) { return TypedResults.NotFound(); }
+
+    labels.Remove(labelToRemove);
+    return TypedResults.Ok();
+})
+.WithName("RemoveLabels")
+.WithOpenApi();
 
 app.Run();
